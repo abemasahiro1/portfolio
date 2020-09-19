f1dc75d7a4af5d3ad9925f0b24061c6a9706f444
abemasahiro1 <misutiru2468@gmail.com>
2020-09-19 09:30:21 +0900

    xcodoのプロジェクトを作成

---
+++ portfolio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  portfolio
+//
+//  Created by アベマサヒロ on 2020/09/19.
+//  Copyright © 2020 wings. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
