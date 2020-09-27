e6669455b8f2937ad0e43aba62ec3127e3cf5c16
abemasahiro1 <misutiru2468@gmail.com>
2020-09-27 14:02:16 +0900

    データ作成

---
+++ portfolio/Produce.swift
@@ -0,0 +1,16 @@
+//
+//  Produce.swift
+//  portfolio
+//
+//  Created by アベマサヒロ on 2020/09/27.
+//  Copyright © 2020 wings. All rights reserved.
+//
+
+import Foundation
+
+struct Produce  {
+    var month:String
+    var name:String
+    var body:String
+    
+}
