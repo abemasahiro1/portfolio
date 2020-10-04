19dfa472a18d778ac6fa8cc798f01e977724c1e2
abemasahiro1 <misutiru2468@gmail.com>
2020-10-04 10:37:46 +0900

    Button削除　画面遷移変更

--- portfolio/ViewController.swift
+++ portfolio/ViewController.swift
@@ -14,6 +14,14 @@ class ViewController: UIViewController,FSCalendarDataSource, FSCalendarDelegate,
     @IBOutlet weak var calendar: FSCalendar!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        calendar.calendarWeekdayView.weekdayLabels[0].text = "日"
+        calendar.calendarWeekdayView.weekdayLabels[1].text = "月"
+        calendar.calendarWeekdayView.weekdayLabels[2].text = "火"
+        calendar.calendarWeekdayView.weekdayLabels[3].text = "水"
+        calendar.calendarWeekdayView.weekdayLabels[4].text = "木"
+        calendar.calendarWeekdayView.weekdayLabels[5].text = "金"
+        calendar.calendarWeekdayView.weekdayLabels[6].text = "土"
         // Do any additional setup after loading the view.
         calendar.delegate = self
         calendar.dataSource = self
@@ -24,5 +32,8 @@ class ViewController: UIViewController,FSCalendarDataSource, FSCalendarDelegate,
            produceViewController.monthInt = month
       }
 
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        performSegue(withIdentifier: "goProduce", sender: nil)
+    }
 }
 
