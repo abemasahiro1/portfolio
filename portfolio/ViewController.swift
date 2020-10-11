9d626b3b441a4f417aee66bf1467a7fdaf3e616e
abemasahiro1 <misutiru2468@gmail.com>
2020-10-11 10:38:36 +0900

    月によって色を変更する

--- portfolio/ViewController.swift
+++ portfolio/ViewController.swift
@@ -36,7 +36,35 @@ class ViewController: UIViewController,FSCalendarDataSource, FSCalendarDelegate,
         performSegue(withIdentifier: "goProduce", sender: nil)
     }
     func calendarCurrentPageDidChange(_ calendar: FSCalendar) {
-        self.view.backgroundColor = UIColor.red
+             switch Calendar.current.component(.month, from: calendar.currentPage) {
+            case 1:
+                self.view.backgroundColor = UIColor.lightGray
+            case 2:
+                self.view.backgroundColor = UIColor.gray
+            case 3:
+                self.view.backgroundColor = UIColor.darkGray
+            case 4:
+                self.view.backgroundColor = UIColor.systemPink
+            case 5:
+                self.view.backgroundColor = UIColor.red
+            case 6:
+                self.view.backgroundColor = UIColor.orange
+            case 7:
+                self.view.backgroundColor = UIColor.yellow
+            case 8:
+                self.view.backgroundColor = UIColor.systemYellow
+            case 9:
+                self.view.backgroundColor = UIColor.purple
+            case 10:
+                self.view.backgroundColor = UIColor.black
+            case 11:
+                self.view.backgroundColor = UIColor.blue
+            case 12:
+                self.view.backgroundColor = UIColor.white
+            default:
+                break
+            }
     }
-}
+    }
+
 
