84f0f88252522d37a488d1f84cfe9344cf702c04
abemasahiro1 <misutiru2468@gmail.com>
2020-10-18 09:46:26 +0900

    月の季節の作成

--- portfolio/ViewController.swift
+++ portfolio/ViewController.swift
@@ -10,8 +10,10 @@ import UIKit
 import FSCalendar
 
 class ViewController: UIViewController,FSCalendarDataSource, FSCalendarDelegate, FSCalendarDelegateAppearance {
-
+    
     @IBOutlet weak var calendar: FSCalendar!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,53 +24,45 @@ class ViewController: UIViewController,FSCalendarDataSource, FSCalendarDelegate,
         calendar.calendarWeekdayView.weekdayLabels[4].text = "木"
         calendar.calendarWeekdayView.weekdayLabels[5].text = "金"
         calendar.calendarWeekdayView.weekdayLabels[6].text = "土"
+        imageView.alpha = 0.2
+        imageView.contentMode = .scaleAspectFill
         // Do any additional setup after loading the view.
         calendar.delegate = self
         calendar.dataSource = self
+        changeSeaon()
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-           let produceViewController = segue.destination as! ProduceViewController
-           let month = Calendar.current.component(.month, from: calendar.currentPage)
-           produceViewController.monthInt = month
-      }
-
-    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
-        performSegue(withIdentifier: "goProduce", sender: nil)
+        let produceViewController = segue.destination as! ProduceViewController
+        let month = Calendar.current.component(.month, from: calendar.currentPage)
+        produceViewController.monthInt = month
     }
-    func calendarCurrentPageDidChange(_ calendar: FSCalendar) {
-        
+    
+    
+    private func changeSeaon(){
         UIView.animate(withDuration: 1.0, delay: 0.0, animations: {
-            switch Calendar.current.component(.month, from: calendar.currentPage) {
-            case 1:
-                self.view.backgroundColor = UIColor.lightGray
-            case 2:
-                self.view.backgroundColor = UIColor.gray
-            case 3:
-                self.view.backgroundColor = UIColor.darkGray
-            case 4:
-                self.view.backgroundColor = UIColor.systemPink
-            case 5:
-                self.view.backgroundColor = UIColor.red
-            case 6:
-                self.view.backgroundColor = UIColor.orange
-            case 7:
-                self.view.backgroundColor = UIColor.yellow
-            case 8:
-                self.view.backgroundColor = UIColor.systemYellow
-            case 9:
-                self.view.backgroundColor = UIColor.purple
-            case 10:
-                self.view.backgroundColor = UIColor.black
-            case 11:
-                self.view.backgroundColor = UIColor.blue
-            case 12:
-                self.view.backgroundColor = UIColor.white
+            switch Calendar.current.component(.month, from: self.calendar.currentPage) {
+            case 3, 4, 5:
+                self.imageView.image = UIImage(named: "春")
+            case 6, 7, 8:
+                self.imageView.image = UIImage(named: "夏")
+            case 9, 10, 11:
+                self.imageView.image = UIImage(named: "秋")
+            case 12, 1, 2:
+                self.imageView.image = UIImage(named: "冬")
+                
             default:
                 break
             }
         }, completion:nil)
+    }
+    
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        performSegue(withIdentifier: "goProduce", sender: nil)
+    }
+    func calendarCurrentPageDidChange(_ calendar: FSCalendar) {
+        changeSeaon()
         
     }
-    }
+}
 
 
